81901d4740f68ce9f4bae8cffd9e9164e8d1f91d
elkanajnrmanullang <elkanamanullang7@gmail.com>
2026-05-22 21:51:23 +0700

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
-// Menggunakan font Inter sebagai standar UI
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
